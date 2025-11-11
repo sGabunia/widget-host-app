75aebd5696ae43792bfbd2d20be4d4951f2cafb6
Sergo Gabunia <sgabunia@upgaming.com>
2025-11-11 10:06:28 +0400

    Update url

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useScript } from "./use-script"
 
 function App() {
-  const status = useScript("https://chat.nebi.dev/v2/injector/", {
+  const status = useScript("https://chat.nebi.live/v2/injector/", {
     "data-brand-id": "10",
     "data-host": "live"
   })
